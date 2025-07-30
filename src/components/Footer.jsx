b6fee357b20eb5341fcfcf16c4d4ec4272ba002a
CryptoKage <chris_aze@hotmail.com>
2025-07-30 04:17:37 +0100

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,24 @@
 
 import React from 'react';
 
-// --- NEW --- Import the SVG files as components
-import { ReactComponent as XIcon } from '../assets/x-icon.svg';
-import { ReactComponent as TelegramIcon } from '../assets/telegram.svg';
+// --- NEW, MORE RELIABLE IMPORT METHOD ---
+import xIconPath from '../assets/x-icon.svg';
+import telegramIconPath from '../assets/telegram.svg';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__contact">Contact: info@hyperstrategies.xyz</div>
       
-      {/* --- NEW Social Links Section --- */}
       <div className="footer__socials">
         <span>Follow us:</span>
         {/* Replace '#' with your actual social media URLs */}
         <a href="#" target="_blank" rel="noopener noreferrer" className="social-link" title="Follow us on X">
-          <XIcon />
+          {/* --- The SVG is now used in a standard <img> tag --- */}
+          <img src={xIconPath} alt="X Icon" className="social-icon" />
         </a>
         <a href="#" target="_blank" rel="noopener noreferrer" className="social-link" title="Join our Telegram">
-          <TelegramIcon />
+          <img src={telegramIconPath} alt="Telegram Icon" className="social-icon" />
         </a>
       </div>
 
