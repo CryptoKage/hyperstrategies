c0dc6e57f207efa4ad11a0a349a4573b3b9603dc
CryptoKage <chris_aze@hotmail.com>
2025-07-31 00:51:34 +0100

    fix: background update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,30 +2,19 @@
 
 import React from 'react';
 import Header from './Header';
-// 1. Import our upgraded footer (remember we renamed it to break the cache)
-import Footer from './Footer'; 
-// 2. Import our two new background effect components
+import Footer from './Footer';
 import InteractiveBackground from './InteractiveBackground';
 import PlasmaEffect from './PlasmaEffect';
 
 const Layout = ({ children }) => {
   return (
-    // The main wrapper for the entire application
     <div className="layout-wrapper">
-      
-      {/* 3. The background effects are placed here, at the top level */}
-      {/* They will sit behind all other content on every page */}
       <PlasmaEffect />
       <InteractiveBackground />
-      
       <Header />
-      
-      {/* The 'main' element is where your page-specific content will render */}
-      <main className="main-content">
+      <main className="main-content"> {/* Ensure this class is here */}
         {children}
       </main>
-
-      {/* 4. We now use our proper, upgraded footer component */}
       <Footer />
     </div>
   );
