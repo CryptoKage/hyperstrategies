ad01470c6e6e5fc0f0a7c9241fda3991d00bc285
CryptoKage <chris_aze@hotmail.com>
2025-08-04 14:40:20 +0100

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,35 @@
 
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   const { t } = useTranslation();
 
   return (
     <footer className="footer">
-      {/* Contact info removed as per your last file version */}
-      
       <div className="footer__socials">
         <span>{t('footer.follow_us')}</span>
         <a href="https://x.com/hyperstrats" target="_blank" rel="noopener noreferrer" className="social-link" title="Follow us on X">
-          <img src="/icons/x-icon-black.svg" alt="X Icon" className="social-icon" />
+          <img src="/icons/x-icon.png" alt="X Icon" className="social-icon" />
         </a>
         <a href="https://t.me/hyperstrategies" target="_blank" rel="noopener noreferrer" className="social-link" title="Join our Telegram">
-          <img src="/icons/telegram-icon.svg" alt="Telegram Icon" className="social-icon" />
+          <img src="/icons/telegram-icon.png" alt="Telegram Icon" className="social-icon" />
         </a>
       </div>
 
       <div className="footer__legal">
-        <a href="#" target="_blank" rel="noopener noreferrer">{t('footer.documentation')}</a>
+        <a href="https://hyper-strategies.gitbook.io/hyper-strategies-docs/" target="_blank" rel="noopener noreferrer">
+          {t('footer.documentation', 'Docs')}
+        </a>
         <span className="separator">|</span>
-        <a href="#" target="_blank" rel="noopener noreferrer">{t('footer.terms')}</a>
+        <a href="https://hyper-strategies.gitbook.io/hyper-strategies-docs/user-guide/user-guide-getting-started" target="_blank" rel="noopener noreferrer">
+          {t('footer.user_guide', 'User Guide')}
+        </a>
+        <span className="separator">|</span>
+        <Link to="/fees">{t('fees_page.nav_fees', 'Fees')}</Link>
+        <span className="separator">|</span>
+        <Link to="/legal">{t('footer.terms_link', 'Legal')}</Link>
       </div>
     </footer>
   );
