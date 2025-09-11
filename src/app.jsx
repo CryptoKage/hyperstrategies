799ec3b6f6ac56dc5b8660e4326186c34c8b9504
CryptoKage <chris_aze@hotmail.com>
2025-09-11 17:06:49 +0100

    upgrade 0.9.2

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import Router from './pages/Router';
+import './styles/global.css';
+import Header from './components/Header';
+import Hero from './components/Hero'; // You will need to re-create or find this component
+import CardSection from './components/CardSection'; // You will need to re-create or find this component
+import Footer from './components/Footer';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <CardSection />
+      <Footer />
+    </>
+  );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+t
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import './styles/global.css';
-import App from './app';
+import Router from './pages/Router'; // Changed from App
 import './i18n';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
@@ -12,10 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Router />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
 serviceWorkerRegistration.register();
