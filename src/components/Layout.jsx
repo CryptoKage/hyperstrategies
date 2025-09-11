82f725ef5c33126c36c017ed307994f96d747da6
CryptoKage <chris_aze@hotmail.com>
2025-09-11 13:21:29 +0100

    upgrade 0.9.2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,11 +10,11 @@ import PlasmaEffect from './PlasmaEffect';
 const Layout = ({ children, showInteractiveBackground = true }) => {
   return (
     // We add a new class 'has-animated-bg' to act as our hook
-    <div className="layout-wrapper has-animated-bg">
+        <div className="layout-wrapper has-animated-bg relative flex min-h-screen flex-col bg-[#040e21]">
       <PlasmaEffect />
       {showInteractiveBackground && <InteractiveBackground />}
       <Header />
-      <main className="main-content">
+      <main className="main-content" flex-1>
         {children}
       </main>
       <Footer />
